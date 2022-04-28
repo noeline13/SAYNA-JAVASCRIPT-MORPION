9b97616cdc786f4614980d9c07862866488bfcfd
Noeline <noeline.ravoniarisoa7@gmail.com>
2022-04-28 15:44:10 +0300

    finition

--- scripts.js
+++ scripts.js
@@ -19,6 +19,7 @@ window.onload = () => {
 selectXBtn.onclick = () => {
     selectBox.classList.add("hide");
     playBoard.classList.add("show");
+    players.setAttribute("class", "range active player")
 
 }
 
@@ -31,44 +32,91 @@ selectOBtn.onclick = () => {
 let playerO = "O";
 let playerX = "X";
 let playerSign = "X";
+let runBot = true;
 
-
-// recupere les case
+// function pour le joueur
 function chooseCase(element) {
     if (players.classList.contains("player")) {
-        element.innerHTML = playerO;
-        players.classList.add("active");
-    } else {
+        playerSign = "O";
         element.innerHTML = playerX;
         players.classList.add("active");
+        element.setAttribute("id", playerSign)
+    } else {
+        element.innerHTML = playerO;
+        players.classList.add("active");
+        element.setAttribute("id", playerSign);
     }
+    selectWinner();
+    playBoard.style.pointerEvents = "none";
     element.style.pointerEvents = "none";
     let randomDelayTime = ((Math.random() * 1000) + 200).toFixed();
     setTimeout(() => {
-        bot();
+        bot(runBot);
     }, randomDelayTime);
 
-};
+}
 
 
 // function auto selection (CPU)
-function bot() {
-    let array = [];
-    for (let i = 0; i < allBox.length; i++) {
-        if (allBox[i].childElementCount == 0) {
-            array.push(i);
+function bot(runBot) {
+    if (runBot) {
+        playerSign = "O";
+        let array = [];
+        for (let i = 0; i < allBox.length; i++) {
+            if (allBox[i].childElementCount == 0) {
+                array.push(i);
+            }
         }
-    }
-    let randomBox = array[Math.floor(Math.random() * array.length)];
-    console.log(randomBox);
-    if (array.length > 0) {
-        if (players.classList.contains("player")) {
-            allBox[randomBox].innerHTML = playerX;
-            players.classList.remove("active");
-        } else {
-            allBox[randomBox].innerHTML = playerO;
-            players.classList.remove("active");
+        let randomBox = 0;
+        do {
+            randomBox = array[Math.floor(Math.random() * array.length)];
+        } while (allBox[randomBox].textContent !== "")
+        console.log(randomBox);
+        if (array.length > 0) {
+            if (players.classList.contains("player")) {
+                playerSign = "X";
+                allBox[randomBox].innerHTML = playerO;
+                players.classList.remove("active");
+                allBox[randomBox].setAttribute("id", playerSign);
+            } else {
+                allBox[randomBox].innerHTML = playerX;
+                players.classList.add("active");
+                allBox[randomBox].setAttribute("id", playerSign);
+            }
+            selectWinner();
         }
+        allBox[randomBox].style.pointerEvents = "none";
+        playBoard.style.pointerEvents = "auto";
+        playerSign = "X";
     }
-    allBox[randomBox].style.pointerEvents = "none";
+}
+
+// definir le gagnant
+function getClass(classname) {
+    return document.querySelector(".item" + classname).id;
+}
+
+function ckeckThreeClasses(val1, val2, val3, sign) {
+    if (getClass(val1) == sign && getClass(val2) == sign && getClass(val3) == sign) {
+        return true;
+    }
+}
+
+function selectWinner() {
+    if (ckeckThreeClasses(1, 2, 3, playerSign) || ckeckThreeClasses(4, 5, 6, playerSign) || ckeckThreeClasses(7, 8, 9, playerSign) || ckeckThreeClasses(1, 4, 3, playerSign) || ckeckThreeClasses(2, 5, 8, playerSign) || ckeckThreeClasses(3, 6, 9, playerSign) || ckeckThreeClasses(1, 5, 9, playerSign) || ckeckThreeClasses(3, 5, 7, playerSign)) {
+        console.log(playerSign + " " + "is the winner")
+        runBot = false;
+        bot(runBot);
+        setTimeout(() => {
+            playBoard.classList.remove("show");
+            resultBox.classList.add("show");
+        }, 700);
+        wonText.innerHTML = 'VICTORY'
+    }
+}
+
+
+// resulta
+replayBtn.onclick = () => {
+    window.location.reload();
 }
